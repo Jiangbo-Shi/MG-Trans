30ea4219db0b769ffa1e4882c84fcbb5632c9418
Jiangbo Shi <shijiangbo@stu.xjtu.edu.cn>
2024-01-22 21:09:54 +0800

    Update patch_extraction.sh

--- feature_extraction/patch_extraction.sh
+++ feature_extraction/patch_extraction.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=0
 python patch_extraction.py \
 --patches_path '/data_prepare/BRIGHT/patches_512' \
 --library_path 'patch_feature_save_path' \
---model_name 'clip_ViTB32' \
+--model_name 'resnet50_trunc' \
 --batch_size 64 \
